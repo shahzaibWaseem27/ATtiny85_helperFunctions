b447f26120e63f28ef486d3e58f4929beb1db071
shahzaibWaseem27 <110247209+shahzaibWaseem27@users.noreply.github.com>
2024-08-21 22:35:40 +0200

    Bit masking mistakes fixed, etc.

--- PWM/pwm.h
+++ PWM/pwm.h
@@ -1,138 +1,90 @@
 #include <avr/io.h>
 
-void setTimerMode(char mode){
+void setTimerMode(char mode) {
+    switch (mode) {
+        case 'n':
+        case 'N':
+            // Normal mode
+            TCCR0A &= ~((1 << WGM00) | (1 << WGM01));
+            TCCR0B &= ~(1 << WGM02);
+            break;
 
-  switch(mode){
+        case 'f':
+        case 'F':
+            // Fast PWM mode
+            TCCR0A |= (1 << WGM00) | (1 << WGM01);
+            TCCR0B &= ~(1 << WGM02);
+            break;
 
-    case 'n':
-    case 'N':
-
-      TCCR0A &= ~((1 << WGM02) & (1 << WGM01) & (1 << WGM00));
-      break;
-
-    case 'f':
-    case 'F':
-
-      TCCR0A &= ~(1 << WGM02);
-      TCCR0A |= (1 << WGM01) | (1 << WGM00);
-      break;
-
-  }
-
+        // Add more modes if needed
+    }
 }
 
-void setTimerPrescalar(int prescalar){
+void setTimerPrescalar(int prescalar) {
+    switch (prescalar) {
+        case 1:
+            TCCR0B &= ~((1 << CS02) | (1 << CS01));
+            TCCR0B |= (1 << CS00);
+            break;
 
-  switch(prescalar){
+        case 8:
+            TCCR0B &= ~((1 << CS02) | (1 << CS00));
+            TCCR0B |= (1 << CS01);
+            break;
 
-    case 1:
+        case 64:
+            TCCR0B &= ~(1 << CS02);
+            TCCR0B |= (1 << CS01) | (1 << CS00);
+            break;
 
-      TCCR0B &= ~(1 << CS02);
-      TCCR0B &= ~(1 << CS01);
-      TCCR0B |= (1 << CS00);
-      break;
-    
-    case 8:
+        case 256:
+            TCCR0B |= (1 << CS02);
+            TCCR0B &= ~((1 << CS01) | (1 << CS00));
+            break;
 
-      TCCR0B &= ~(1 << CS02);
-      TCCR0B |= (1 << CS01);
-      TCCR0B &= ~(1 << CS00);
-      break;
-    
-    case 64:
+        case 1024:
+            TCCR0B |= (1 << CS02);
+            TCCR0B &= ~(1 << CS01);
+            TCCR0B |= (1 << CS00);
+            break;
 
-      TCCR0B &= ~(1 << CS02);
-      TCCR0B |= (1 << CS01);
-      TCCR0B |= (1 << CS00);
-      break;
-
-    case 256:
-
-      TCCR0B |= (1 << CS02);
-      TCCR0B &= ~(1 << CS01);
-      TCCR0B &= ~(1 << CS00);
-      break;
-
-    case 1024:
-
-      TCCR0B |= (1 << CS02);
-      TCCR0B &= ~(1 << CS01);
-      TCCR0B |= (1 << CS00);
-      break;
-
-  }
-
+        // Add more prescalars if needed
+    }
 }
 
-void configureLogicChangePWM(char logicChange, char PWMpin){
-
-  switch(logicChange){
-
-    case 's':
-    case 'S':
-
-        switch(PWMpin){
-
-            case 'A':
-            case 'a':
-
+void configureLogicChangePWM(char logicChange, char PWMpin) {
+    switch (logicChange) {
+        case 's':
+        case 'S':  // Set on match
+            if (PWMpin == 'A' || PWMpin == 'a') {
                 TCCR0A |= (1 << COM0A1) | (1 << COM0A0);
-                break;
-
-            case 'B':
-            case 'b':
-
+            } else if (PWMpin == 'B' || PWMpin == 'b') {
                 TCCR0A |= (1 << COM0B1) | (1 << COM0B0);
-                break;
+            }
+            break;
 
-        }
-
-        break;
-
-    case 'r':
-    case 'R':
-        switch(PWMpin){
-
-            case 'A':
-            case 'a':
-
+        case 'r':
+        case 'R':  // Clear on match
+            if (PWMpin == 'A' || PWMpin == 'a') {
                 TCCR0A |= (1 << COM0A1);
                 TCCR0A &= ~(1 << COM0A0);
-                break;
-
-            case 'B':
-            case 'b':
-
+            } else if (PWMpin == 'B' || PWMpin == 'b') {
                 TCCR0A |= (1 << COM0B1);
                 TCCR0A &= ~(1 << COM0B0);
-                break;
+            }
+            break;
 
-        }
-
-        break;   
-
-  }
-
+        // Add more modes if needed
+    }
 }
 
-void setPWMDutyCycle(int dutyCycleVal, char pinPWM){
+void setPWMDutyCycle(int dutyCycleVal, char pinPWM) {
+    // Correct calculation of the ratio for the 8-bit timer
+    int ratioForMCU = (dutyCycleVal * 255) / 100;
 
-  int ratioForMCU = (dutyCycleVal/100) * 255;
-
-  switch(pinPWM){
-
-    case 'a':
-    case 'A':
-
-      OCR0A = ratioForMCU;
-      break;
-
-    case 'b':
-    case 'B':
-
-      OCR0B = ratioForMCU;
-      break;
-
-  }
-
+    if (pinPWM == 'A' || pinPWM == 'a') {
+        OCR0A = ratioForMCU;
+    } else if (pinPWM == 'B' || pinPWM == 'b') {
+        OCR0B = ratioForMCU;
+    }
 }
